a034700156768ea4a733249f752da0a799496499
sultan-710 <omari.sultan2000@gmail.com>
2023-04-12 15:09:42 +0200

    test test test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,23 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illum
+
+inate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Fou
 
 
 
 ndation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\R
+use Illuminate\
 
-
-
-
-
-
-outing\Controller as BaseController;
-
-class Contro
 
 ller extends BaseCon
 
