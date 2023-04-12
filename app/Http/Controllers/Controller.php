d4ca7335feed398bfc5c7cef91fce6e279db0a9f
sultan-710 <omari.sultan2000@gmail.com>
2023-04-12 14:43:23 +0200

    hdhdffsddsfsd

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Fou
+
+
+
+ndation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
